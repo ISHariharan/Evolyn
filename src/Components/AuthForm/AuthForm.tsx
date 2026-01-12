2902f9b5539bbde7c4b25ce848132b42ec5b39f5
ISHariharan <ishariharan2004@gmail.com>
2026-01-12 16:57:55 +0530

    Change : PopUp implementation completed

--- src/Components/AuthForm/AuthForm.tsx
+++ src/Components/AuthForm/AuthForm.tsx
@@ -1,35 +1,40 @@
 import "./AuthForm.scss";
 
-const AuthForm = () => {
+const AuthForm = ({isOpen, onClose}) => {
+    if(!isOpen) return;
     return (
-        <form className="form">
-            <p className="title">Register </p>
-            <p className="message">Signup now and get full access to our app. </p>
-            <div className="flex">
-            <label>
-                <input className="input" type="text" placeholder="firstname" required />
-                <span>Firstname</span>
-            </label>
-            <label>
-                <input className="input" type="text" placeholder="lastname" required />
-                <span>Lastname</span>
-            </label>
-            </div>  
-            <label>
-            <input className="input" type="email" placeholder="Email" required />
-            <span>Email</span>
-            </label> 
-            <label>
-            <input className="input" type="password" placeholder="Password" required />
-            <span>Password</span>
-            </label>
-            <label>
-            <input className="input" type="password" placeholder="Confirm Password" required />
-            <span>Confirm password</span>
-            </label>
-            <button className="submit">Submit</button>
-            <p className="signin">Already have an acount ? <a href="#">Signin</a> </p>
-      </form>
+        <div className="AuthForm-Overlay">
+            <div className="AuthForm-PopUp">
+                <form className="form">
+                    <p className="AuthForm-Title">Register </p>
+                    <p className="message">Signup now and get full access to our app. </p>
+                    <div className="flex">
+                    <label>
+                        <input className="input" type="text" placeholder="" required />
+                        <span>Firstname</span>
+                    </label>
+                    <label>
+                        <input className="input" type="text" placeholder="" required />
+                        <span>Lastname</span>
+                    </label>
+                    </div>  
+                    <label>
+                        <input className="input" type="email" placeholder="" required />
+                        <span>Email</span>
+                    </label> 
+                    <label>
+                        <input className="input" type="password" placeholder="" required />
+                        <span>Password</span>
+                    </label>
+                    <label>
+                        <input className="input" type="password" placeholder="" required />
+                        <span>Confirm password</span>
+                    </label>
+                    <button className="submit" onClick={onClose}>Submit</button>
+                    <p className="signin">Already have an acount ? <a href="#">Signin</a> </p>
+                </form>
+            </div>
+        </div>
     );
 }
 
