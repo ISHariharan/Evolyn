afdb0ca5888ee38da76aaebfee228443ce43edff
ISHariharan <ishariharan2004@gmail.com>
2026-01-13 11:51:34 +0530

    Change : AuthForm responsive change for SignIn and SignUp

--- src/Components/AuthForm/AuthForm.tsx
+++ src/Components/AuthForm/AuthForm.tsx
@@ -1,26 +1,40 @@
 import "./AuthForm.scss";
+import { useState, useEffect } from "react";
 
 const AuthForm = ({isOpen, onClose}) => {
+    const [authFormType, setAuthFormType] = useState<string>("SignUp");
+    const [authFormDetails, setAuthFormDetails] = useState<boolean>(true);
+    let oppAuthFormType = authFormType === 'SignUp' ? 'SignIn' : 'SignUp';
+
+    const swapAuthForm = (authFormSelected) => {
+        setAuthFormType(authFormSelected);
+        oppAuthFormType = authFormSelected === 'SignUp' ? 'SignIn' : 'SignUp';
+        setAuthFormDetails(!authFormDetails);
+    }
+
     if(!isOpen) return;
+
     return (
         <div className="AuthForm-Overlay">
             <div className="AuthForm-PopUp">
-                <form className="form">
+                <form className="Authform">
                     <div className="AuthForm-header-text">
-                        <div><p className="AuthForm-Title">Register </p></div>
+                        <div><p className="AuthForm-Title">{authFormType === 'SignUp' ? 'Register' : 'Login'} </p></div>
                         <div><button className="AuthForm-close-icon bx bx-x-circle" onClick={onClose}></button></div>
                     </div>
-                    <p className="message">Signup now and get full access to our app. </p>
-                    <div className="flex">
-                    <label>
-                        <input className="input" type="text" placeholder="" required />
-                        <span>Firstname</span>
-                    </label>
-                    <label>
-                        <input className="input" type="text" placeholder="" required />
-                        <span>Lastname</span>
-                    </label>
-                    </div>  
+                    <p className="message">{authFormType} now and get full access to our app. </p>
+                    {authFormDetails && (
+                        <div className="flex">
+                            <label>
+                                <input className="input" type="text" placeholder="" required />
+                                <span>Firstname</span>
+                            </label>
+                            <label>
+                                <input className="input" type="text" placeholder="" required />
+                                <span>Lastname</span>
+                            </label>
+                        </div>
+                    )}  
                     <label>
                         <input className="input" type="email" placeholder="" required />
                         <span>Email</span>
@@ -29,12 +43,14 @@ const AuthForm = ({isOpen, onClose}) => {
                         <input className="input" type="password" placeholder="" required />
                         <span>Password</span>
                     </label>
-                    <label>
-                        <input className="input" type="password" placeholder="" required />
-                        <span>Confirm password</span>
-                    </label>
+                    {authFormDetails && (
+                        <label>
+                            <input className="input" type="password" placeholder="" required />
+                            <span>Confirm password</span>
+                        </label>
+                    )}
                     <button className="submit" onClick={onClose}>Submit</button>
-                    <p className="signin">Already have an acount ? <a href="#">Signin</a> </p>
+                    <p className="AuthForm-signin">Already have an acount ? <button onClick={() => swapAuthForm(oppAuthFormType)} className="AuthForm-SignIn-Form">{oppAuthFormType}</button> </p>
                 </form>
             </div>
         </div>
