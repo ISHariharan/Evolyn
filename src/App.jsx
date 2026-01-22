bfff3a500ce36112dc9905b2e072750d08a0dd01
ISHariharan <ishariharan2004@gmail.com>
2026-01-22 13:57:12 +0530

    Change : Temporary Routing for toast messages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Home from './Components/Home/Home';
 import NavBar from './Components/NavBar/NavBar';
 import Header from './Components/Header/Header';
 import Explore from './Components/Explore/Explore';
+import SuccessToastMessage from "./Common/SuccessToastMessage/SuccessToastMessage";
+import ErrorToastMessage from "./Common/ErrorToastMessage/ErrorToastMessage";
+import InfoToastMessage from "./Common/InfoToastMessage/InfoToastMessage";
+import WarningToastMessage from "./Common/WarningToastMessage/WarningToastMessage";
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/explore" element={<Explore />} />
+          <Route path="/success" element={<SuccessToastMessage />} />
+          <Route path="/error" element={<ErrorToastMessage/>} />
+          <Route path="/info" element={<InfoToastMessage/>} />
+          <Route path="/warning" element={<WarningToastMessage />} />
         </Routes>
         <Header/>
         <NavBar/>
