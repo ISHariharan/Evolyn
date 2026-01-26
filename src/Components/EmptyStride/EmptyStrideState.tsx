c484691594c7bd567eacb7f62d0be2232e96fb85
ISHariharan <ishariharan2004@gmail.com>
2026-01-26 12:23:35 +0530

    Change : Integrated create pop up into Empty Stride State

--- src/Components/EmptyStride/EmptyStrideState.tsx
+++ src/Components/EmptyStride/EmptyStrideState.tsx
@@ -1,7 +1,13 @@
 import "./EmptyStrideState.scss";
 import { ReactComponent as EmptyStateImage } from "../../Statics/StrideEmptyState.svg";
+import CreateStrideWorkspaceDialog from "../CreateStrideWorkspaceDialog/CreateStrideWorkspaceDialog";
+import { useState } from "react";
 
 const EmptyStrideState = () => {
+    const [createWorkspacePopUp, setCreateWorkspacePopUp] = useState<boolean>(false);
+    const handleWorkspaceCreation = () => {
+        setCreateWorkspacePopUp(true);
+    }
     return (
         <div className="EmptyStride-Container">
             <h1 className="EmptyStride-Heading">You've not started your WorkSpace</h1>
@@ -12,15 +18,16 @@ const EmptyStrideState = () => {
             <div className="relative group">
                 <button
                     className="
-                    relative inline-block p-px font-semibold leading-6
-                    shadow-2xl cursor-pointer rounded-xl shadow-zinc-900
-                    transition-transform duration-300 ease-in-out
-                    hover:scale-105 active:scale-95
+                        relative inline-block p-px font-semibold leading-6
+                        shadow-2xl cursor-pointer rounded-xl shadow-zinc-900
+                        transition-transform duration-300 ease-in-out
+                        hover:scale-105 active:scale-95
                     "
                     style={{
-                    background: "var(--button-color)",
-                    color: "var(--button-text-color)",
+                        background: "var(--button-color)",
+                        color: "var(--button-text-color)",
                     }}
+                    onClick={handleWorkspaceCreation}
                 >
                     {/* Glow / Gradient Border */}
                     <span
@@ -44,7 +51,7 @@ const EmptyStrideState = () => {
                     >
                     <div className="relative z-10 flex items-center space-x-2">
                         <span className="transition-all duration-500 group-hover:translate-x-1">
-                            Start you Stride WorkSpace
+                            Start your Stride WorkSpace
                         </span>
 
                         <svg
@@ -64,6 +71,9 @@ const EmptyStrideState = () => {
                     </span>
                 </button>
             </div>
+            {createWorkspacePopUp && (
+                <CreateStrideWorkspaceDialog onClose={() => setCreateWorkspacePopUp(false)} />
+            )}
         </div>
     );
 }
