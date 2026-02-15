a0a6c3349077ca24a18a30e74023e16f92700f74
ISHariharan <ishariharan2004@gmail.com>
2026-02-15 18:02:38 +0530

    Change : Newly relocated Home.tsx and Newly added NavBarToggle SVG

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,17 @@
+import "./Home.scss"
+
+const Home = () => {
+    return(
+        <div>
+            <div className="Home">
+                <section>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt vel illum fuga unde cum, voluptates magni molestias eveniet culpa autem ut, totam veniam, suscipit tempore ullam pariatur est at asperiores?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt vel illum fuga unde cum, voluptates magni molestias eveniet culpa autem ut, totam veniam, suscipit tempore ullam pariatur est at asperiores?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt vel illum fuga unde cum, voluptates magni molestias eveniet culpa autem ut, totam veniam, suscipit tempore ullam pariatur est at asperiores?</p>
+                </section>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
